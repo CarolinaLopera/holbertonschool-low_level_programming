7c2c29356970fca99d4dd7e4c6cc717277427631
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-18 04:44:26 +0000

    print whit putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	int putchar(int str);
+	return (1);
+}
