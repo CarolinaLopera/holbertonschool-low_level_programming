868a84e33ff68821650b46f59ddbcbc9e26ea444
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-18 05:42:56 +0000

    print whit fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(str, stdout);
 	return (1);
 }
