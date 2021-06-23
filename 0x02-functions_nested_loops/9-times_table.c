91d7ca0d53f01a6d5dafb2e54da24a05fb64f296
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-23 17:09:51 +0000

    tables 0 -9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,28 @@
 */
 void times_table(void)
 {
-	int multi1, multi2;
+	int multi1, multi2, result;
 
 	for (multi1 = 0; multi1 <= 9; multi1++)
 	{
 		for (multi2 = 0; multi2 <= 9; multi2++)
 		{
-			_putchar((multi1 * multi2) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar('\n');
+			result = multi1 * multi2;
+
+			if (result < 10)
+			{
+				_putchar((result) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 }
