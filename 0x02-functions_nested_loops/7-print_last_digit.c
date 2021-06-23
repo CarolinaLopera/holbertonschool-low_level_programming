af8acfc3a0db445e7b6fbb7801d1ccc7cdbf21e2
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-23 00:26:46 +0000

    last digit number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* print_last_digit - check the code for Holberton School students.
+*
+* Return: 0 and 1.
+* @num: is a variable int
+*/
+int print_last_digit(int num)
+{
+	int last_num = num % 10;
+
+	_putchar(last_num);
+}
