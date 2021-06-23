ca9ca4659ab6f08325e88e6240e02be552a77230
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-23 17:39:04 +0000

    suma de enteros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add - check the code for Holberton School students.
+*
+* Return: 0 and 1.
+* @num1: is a variable int.
+* @num2: is a variable int.
+*/
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
