64abe20268e6c9ed116bedd0b87bf258dba2f85a
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-24 20:46:37 +0000

    distingued digit 0 - 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - check the code for Holberton School students.
+ *
+ * Return: Always int.
+ * @a: is a variable int.
+ * @b: is a variable int.
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+}
