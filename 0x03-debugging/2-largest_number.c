88fcb262b8c64c467cede75010e56fa76f110e5a
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-24 17:59:09 +0000

    longer num

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -26,6 +26,25 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	printf("%i", largest);
+	pr_int(largest);
 	return (largest);
+}
+
+/**
+* pr_int - check the code for Holberton School students.
+*
+* Return: int.
+*
+* @n: is a variable int.
+*/
+void pr_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		pr_int(n / 10);
+	_putchar((n % 10) + '0');
 }
