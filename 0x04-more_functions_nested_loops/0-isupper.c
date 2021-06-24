3c42603030307152c05319a02f7458d5d911d951
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-24 20:34:23 +0000

    letter mayus

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isupper - check the code for Holberton School students.
+ *
+ * Return: Always 0 and 1.
+ * @c: is a variable int.
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Continue whit programation c, loops and more loops; functions and more functions.. :D
