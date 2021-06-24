3894ce2f475c96dfdf4c74c4fc253b2a103255a3
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-24 06:43:48 +0000

    table dinamic

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,31 +24,33 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 				}
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((result) + '0');
+				if (multi2 < n)
+				{
+					_putchar(',');
+				}
 			}
 			else if (result > 99)
 			{
+				_putchar(' ');
 				pr_int(result);
+				if (multi2 < n)
+				{
+					_putchar(',');
+				}
 			}
 			else
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
-			}
-			if (multi2 < n && result > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (multi2 < n && result <= 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (multi2 == n && result >= 100)
-			{
-				_putchar(' ');
+				if (multi2 < n)
+				{
+					_putchar(',');
+				}
 			}
 		}
 		_putchar('\n');
