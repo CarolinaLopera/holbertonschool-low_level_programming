19cdcecb74cadf87e66f413ae3dd2ed5520122d6
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-24 20:41:54 +0000

    distingued digit 0 - 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ *
+ * Return: Always 0 and 1.
+ * @c: is a variable int.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
