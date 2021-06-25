457867c67b19d3397f48c2a8f71d86c6b6ca2f1f
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-25 00:13:17 +0000

    multiplo

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - check the code for Holberton School students.
+*
+* Return: void.
+*
+*/
+void main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if (3 % n == 0 && 5 % n == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (5 % n == 0)
+		{
+			printf("Buzz");
+		}
+		else if (3 % n == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%i", n);
+		}
+	}
+	putchar('\n');
+}
