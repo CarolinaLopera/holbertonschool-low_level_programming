0b822b512fd3cfcaf049ca1e24345394b3a69e94
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-25 03:18:09 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+* print_triangle - check the code for Holberton School students.
+*
+* Return: void.
+* @size: is a variable int.
+*/
+void print_triangle(int size)
+{
+	int times;
+	int lines;
+
+	for (times = 1; times <= size; times++)
+	{
+		for (lines = size; lines >= 0; lines--)
+		{
+			if (times > lines)
+			{
+				_putchar(35);
+			}
+			else
+			{
+				_putchar(32);
+			}
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
