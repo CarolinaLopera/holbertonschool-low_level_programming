4c9e4102aade7658bef841e7fdef3adb9467ede3
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-28 20:36:46 +0000

    redeclared variable with point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @n: is a variable point declared.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
