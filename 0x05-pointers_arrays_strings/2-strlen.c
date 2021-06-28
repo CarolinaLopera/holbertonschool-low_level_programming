b8f28b5efabd0f9f1363fd4c1afd4207f9c95f7e
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-28 23:04:39 +0000

    travel array str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @s: is a variable point declared.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < *s; i++)
+	{
+	}
+
+	return (i);
+}
