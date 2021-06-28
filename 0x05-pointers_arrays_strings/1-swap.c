dec75535fc2e11092b07f23ecbf872cddf3216ac
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-28 20:52:38 +0000

    interchange of variables with points

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @a: is a variable point declared.
+ * @b: is a variable point declared.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
