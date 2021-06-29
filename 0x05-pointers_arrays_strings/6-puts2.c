198c0a33a2a895309cb4399f61785337b4a409e2
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-29 20:45:41 +0000

    print array str two in two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (i <= str[i])
+	while (str[i] == '\0' || str[i] != '\0')
 	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
 		_putchar(str[i]);
 		i = i + 2;
 	}
