a8e36341afe5225b634df3a053f445644d3176c2
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-29 20:12:48 +0000

    print array str two in two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @str: is a variable point declared.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
