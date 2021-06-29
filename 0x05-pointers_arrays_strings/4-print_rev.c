dbf7959e8dc8e080ccaad9cb4df515669f51a3d2
Carolina Lopera <carolinalopera08@gmail.com>
2021-06-29 15:34:00 +0000

    print array array reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_rev - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @s: is a variable point declared.
+ */
+void print_rev(char *s)
+{
+	int end;
+	int ini = _strlen(s);
+
+	for (end = ini; end >= 0; end--)
+	{
+		_putchar(s[end]);
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @s: is a variable point declared.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
