429bc2845d2656778e17ed152cecb650f581393e
Carolina Lopera <carolinalopera08@gmail.com>
2021-07-03 00:44:16 +0000

    capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * cap_string - check the code for Holberton School students.
+ *
+ * Return: Always char*
+ * @a: is a variable char*.
+ *
+ */
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (a[i] >= 97 && a[i] <= 122)
+			{
+				a[i] = (a[i] - 32);
+			}
+		}
+		if (a[i] == ' ' || a[i] == ',' || a[i] == '"' || a[i] == '('
+				|| a[i] == '.' || a[i] == ')' || a[i] == ';'
+				|| a[i] == '\n' || a[i] == '!' || a[i] == '?'
+				|| a[i] == '}' || a[i] == '{' || a[i] == '\t')
+		{
+			++i;
+			if (a[i] >= 97 && a[i] <= 122)
+			{
+				a[i] = (a[i] - 32);
+			}
+		}
+	}
+	return (a);
+}
