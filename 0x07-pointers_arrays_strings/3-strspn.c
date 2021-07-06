b196cc7284215e330b0092c73b77230c6e91c709
Carolina Lopera <carolinalopera08@gmail.com>
2021-07-05 23:55:26 -0500

    comparation

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strcat - check the code for Holberton School students.
+ *
+ * Return: Always char*.
+ * @dest: is a variable char*.
+ * @src: is a variable char*.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, bool;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+bool = 1;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+bool = 0;
+break;
+}
+
+}
+if (bool == 1)
+break;
+
+}
+return (i);
+}
