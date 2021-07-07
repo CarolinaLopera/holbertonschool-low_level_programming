067fc199eb46c6b7a403e894b05aa206dcb082ad
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-06 22:07:50 -0500

    print array bidimensional

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 /**
  * print_chessboard - searches a string for any of a set of bytes.
- * @(*a)[8]: is a variable char* bidimensional.
+ * @a: is a variable char* bidimensional.
  * Return: always void.
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            printf("%c", a[i][j]);
-        }
-        printf("\n");
-    }
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
 }
