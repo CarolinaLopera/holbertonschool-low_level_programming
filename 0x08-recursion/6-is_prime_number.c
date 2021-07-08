e231eef8b60f5a5e143e18b0e62e5d2869e0d160
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-07 23:59:54 -0500

    number prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,30 @@
 #include "holberton.h"
 /**
- * _pow_recursion - check the code for Holberton School students.
+ * is_prime_number - check the code for Holberton School students.
  *
  * Return: Always void.
- * @x: is a variable int.
- * @y: is a variable int.
+ * @n: is a variable int.
  */
 int is_prime_number(int n)
 {
-    if (n == 1)
-    {
-        return (0);
-    }
-    if (n == 0)
-    {
-		return (0);
-    }
-	if (n < 0)
-    {
-		return (0);
-    }
-    if (n > 3 && (n % 2) == 0)
-    {
-        return (0);
-    }
-    else
-    {
-        return (1);
-    }
+if (n == 1)
+{
+return (0);
+}
+if (n == 0)
+{
+return (0);
+}
+if (n < 0)
+{
+return (0);
+}
+if (n > 3 && (n % 2) == 0)
+{
+return (0);
+}
+else
+{
+return (1);
+}
 }
