5af04256a65c23bc5127485af65d2e7c519dad8d
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-09 19:51:36 -0500

    num arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always int.
+ * @argc: is a variable int.
+ * @argv: is a variable char*.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	char **aux = argv;
+
+	printf("%i\n", argc);
+	aux++;
+	return (0);
+}
