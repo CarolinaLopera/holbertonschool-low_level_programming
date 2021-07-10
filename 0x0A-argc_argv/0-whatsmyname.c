dc792aa6a596c152583ec0f668353e36320eb916
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-09 19:43:16 -0500

    print name program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,8 @@
  * main - check the code for Holberton School students.
  *
  * Return: Always int.
- * @agrc: is a variable int.
- * @agrv: is a variable char*.
+ * @argc: is a variable int.
+ * @argv: is a variable char*.
  *
  */
 int main(int argc, char *argv[])
