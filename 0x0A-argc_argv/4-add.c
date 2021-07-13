b2ea9ada1a5504cb2e0dcb212fff5098ee50b738
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-12 19:14:43 -0500

    add numbers until argc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+int _isnumber(char *c);
 /**
  * main - check the code for Holberton School students.
  *
@@ -22,7 +23,7 @@ int main(int argc, char *argv[])
 		{
 			int var_atoi = atoi(argv[i]);
 
-			if (var_atoi == 0 && *argv[i] != '0')
+			if ((var_atoi == 0 && *argv[i] != '0') || (_isnumber(argv[i]) == 1))
 			{
 				printf("Error\n");
 				return (1);
@@ -31,6 +32,29 @@ int main(int argc, char *argv[])
 			result += var_atoi;
 		}
 		printf("%i\n", result);
+	}
+	return (0);
+}
+/**
+* _isnumber - check the code for Holberton School students.
+*
+* Return: 0 and 1.
+* @c: is a int assi.
+*/
+int _isnumber(char *c)
+{
+	int i;
+
+	for (i = 0; *c != '\0'; i++)
+	{
+		if (*c >= 48 && *c <= 57)
+		{
+			c++;
+		}
+		else
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
