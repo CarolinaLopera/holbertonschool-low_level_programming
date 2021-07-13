7004c8b0cb585b43029a91e1ba17576072bfe50f
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-13 18:48:42 -0500

    asignation memory malloc and copy aux = str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,6 +19,11 @@ char *_strdup(char *str)
 		char *aux = malloc(*str * sizeof(char));
 		int i;
 
+		if (*aux == NULL)
+		{
+			return ('\0');
+		}
+
 		for (i = 0; str[i] != '\0'; i++)
 		{
 			aux[i] = str[i];
