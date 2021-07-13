77da8bb57dea48783fc34bdfdcde59ba6de13167
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-13 17:12:12 -0500

    create array dinamic

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *aux = malloc(size * sizeof(char));
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (size == 0)
 	{
 		return ('\0');
 	}
+	if (aux == NULL)
+	{
+		return ('\0');
+	}
 
-	for (i = 0; i < size; i++)
+	for (; i < size; i++)
 	{
 		aux[i] = c;
 	}
