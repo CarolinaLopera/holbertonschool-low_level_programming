a32f969129a9cc33162379a442cc8cb87008e140
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-17 16:24:35 -0500

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - check the code for Holberton School students.
+ *
+ * Return: Always null or char pointer.
+ *
+ * @nmemb: is a variable unsigned int.
+ * @size: is a variable unsigned int.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int **asig;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	asig = malloc((nmemb * size) * sizeof(char *));
+
+	if (asig == NULL)
+	{
+		free(asig);
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		asig[i] = 0;
+	}
+	return (asig);
+}
