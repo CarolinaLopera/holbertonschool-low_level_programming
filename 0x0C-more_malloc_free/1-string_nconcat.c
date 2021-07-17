563edf336ee68e6668f8112a7cefd86b5bf1d10f
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-16 19:50:19 -0500

    concatenar two string until n, malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * Return: viod.
+ * @s: is a variable point declared.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+/**
+ * string_nconcat - check the code for Holberton School students.
+ *
+ * Return: Always null or char pointer.
+ * @s1: is a variable char*.
+ * @s2: is a variable char*.
+ * @n: is a variable unsigned int.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = _strlen(s1);
+	unsigned int len2 = _strlen(s2);
+	unsigned int i, j = len1;
+	char *aux;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	aux = malloc((len1 + len2 + 1) * sizeof(char));
+	if (aux == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		aux[i] = s1[i];
+	}
+	for (i = 0; i <= n; i++)
+	{
+		if (len2 != n)
+		{
+			aux[j] = s2[i];
+			j++;
+		}
+		else
+			break;
+	}
+	return (aux);
+}
