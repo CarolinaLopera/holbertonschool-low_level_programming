ea60edcb46ccabc7b9ab37a00deef3d3ab8d110b
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-16 21:24:36 -0500

    concatenar

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -47,19 +47,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
-	{
+
 		aux[i] = s1[i];
-	}
-	for (i = 0; i < n; i++)
+
+	for (i = 0; i < len2; i++)
 	{
+		/*
 		if (i >= len2)
 			break;
-
 		if (len2 != i)
-		{
+			*/
+
 			aux[j] = s2[i];
 			j++;
-		}
 	}
 	aux[j + i] = '\0';
 	return (aux);
