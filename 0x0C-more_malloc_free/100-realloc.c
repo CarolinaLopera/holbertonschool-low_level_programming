49fdcb61d10b6b0a042ff744046e9bf77923dc61
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-17 21:55:15 -0500

    realloc, reasignation of memory malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - check the code for Holberton School students.
+ *
+ * Return: Always null or char pointer.
+ *
+ * @ptr: is a pointer to memory.
+ * @old_size: is a variable unsigned int.
+ * @new_size: is a variable unsigned int.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	ptr = malloc(new_size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
