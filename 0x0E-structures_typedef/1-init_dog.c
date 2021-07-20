0275b1ba22e9b9e6acbefa9a572706eab89222ae
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-19 19:54:25 -0500

    inicializar variables structura

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 /**
  * init_dog - check the code for Holberton School students.
@@ -10,6 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (NULL);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
