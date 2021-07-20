e59d3e0e7cb4baf7b5996262f5e5aa3d9adcfdbd
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-19 19:49:36 -0500

    exercice 0 structur dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - check the code for Holberton School students.
+ *
+ * Return: Always void.
+ * @name: is a variable char pointer of structur dog.
+ * @age: is a variable float of structur dog.
+ * @owner: is a variable char pointer of structur dog.
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
