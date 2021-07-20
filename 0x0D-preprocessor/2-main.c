4090817f7142ff5b86c502c9b8e97b51c4c87290
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-19 21:09:34 -0500

    print file base macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
- * struct dog - check the code for Holberton School students.
+ * main - check the code for Holberton School students.
  *
- * Return: Always void.
- * @name: is a variable char pointer of structur dog.
- * @age: is a variable float of structur dog.
- * @owner: is a variable char pointer of structur dog.
+ * Return: Always 0.
  *
  */
 int main(void)
 {
-    printf(__BASE_FILE__"\n");
-    return (0);
+	printf(__BASE_FILE__"\n");
+	return (0);
 }
