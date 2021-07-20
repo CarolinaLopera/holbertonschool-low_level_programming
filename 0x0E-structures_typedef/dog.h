e3ff9981b96e3ab1db5788d477fe705f2df27dd4
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-19 23:40:47 -0500

    new structur dog_t of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,21 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+typedef struct dog dog_t;
+/**
+ * struct dog_t - check the code for Holberton School students.
+ *
+ * Return: Always void.
+ * @name: is a variable char pointer of structur dog.
+ * @age: is a variable float of structur dog.
+ * @owner: is a variable char pointer of structur dog.
+ *
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
 #endif
