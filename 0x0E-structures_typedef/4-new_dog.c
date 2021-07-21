9b049f50656e8f29c9447e20fe1cded38290e9e7
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-20 22:02:58 -0500

    new dog, copy name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * print_dog - check the code for Holberton School students.
+ * new_dog - check the code for Holberton School students.
  *
  * Return: Always void.
- * @d: is a new structur dog pointer.
+ * @name: is a variable char pointer of structur dog.
+ * @age: is a variable float of structur dog.
+ * @owner: is a variable char pointer of structur dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *nd;
+	dog_t *nd, *aux;
 	int i;
+
 	nd = malloc(sizeof(dog_t));
-	
-	if (nd == NULL)
+	aux = malloc(sizeof(dog_t));
+
+	if (nd == NULL || name == NULL || owner == NULL)
 		return (NULL);
 
 	nd->name = malloc(*name * sizeof(char));
 	if (nd->name == NULL)
 		return (NULL);
-	
+	aux->name = malloc(*name * sizeof(char));
+
 	nd->owner = malloc(*owner * sizeof(char));
 	if (nd->owner == NULL)
 		return (NULL);
-	
+	aux->owner = malloc(*owner * sizeof(char));
+
 	for (i = 0; name[i] != '\0'; i++)
+	{
 		nd->name[i] = name[i];
-	
+		aux->name[i] = name[i];
+	}
+
 	nd->age = age;
 
 	for (i = 0; owner[i] != '\0'; i++)
+	{
 		nd->owner[i] = owner[i];
+		aux->owner[i] = owner[i];
+	}
 
 	return (nd);
 }
