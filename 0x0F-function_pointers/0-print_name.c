8464d4c5f8e7022680086a0e0b89550a1ead9440
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-21 14:46:26 -0500

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,5 @@
 #include "function_pointers.h"
 /**
- * f - check the code for Holberton School students.
- *
- * Return: Always void.
- * @name: is a variable char pointer.
- */
-void f(char *name)
-{
-	int i;
-
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		_putchar(name[i]);
-	}
-}
-/**
  * print_name - check the code for Holberton School students.
  *
  * Return: Always void.
