e9230c9eeadd947c06d65269ec1f2477e5c61022
CarolinaLopera <carolinalopera08@gmail.com>
2021-07-31 13:48:02 -0500

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,20 +4,20 @@
  * print_list - check the code for Holberton School students.
  *
  * Return: Always size_t.
- * @c: is a variable double of structur complex.
+ * @h: points to the next node.
  */
 size_t print_list(const list_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%i] %s\n", h->len, h->str);
-        h = h->next;
-        i++;
-    }
-    return (i);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
