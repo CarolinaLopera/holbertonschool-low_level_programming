483345b27c9d835d87629ccdf1c9ce8ad056d7da
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-02 14:57:45 -0500

    print list int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - This function print a list to integer.
+ *
+ * Return: Always size_t.
+ * @h: points to the first node.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# This other project about linker list in C.
+
+| Function| Description |
+| ------ | ------ |
+| print_listint | This function print a list to integer. Retunr the number of nodes. |
