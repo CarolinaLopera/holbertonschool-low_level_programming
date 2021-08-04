1e190bcc48bbdbee771e67810b91bf1bd9ce4cbf
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-03 19:59:32 -0500

    Fix insert node in specific position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		if ((i + 1) == idx)
 		{
