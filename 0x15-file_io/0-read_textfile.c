d2b1b42954df0577f8804cbc9a39aec5b22624f8
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-09 16:31:17 -0500

    Create file with open and write content

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf  = malloc(letters * sizeof(char));
+	buf = malloc(letters * sizeof(char));
 	if (buf == NULL)
 		return (0);
 
