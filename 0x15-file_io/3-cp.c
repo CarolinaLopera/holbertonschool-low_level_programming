cfc4516c892815c6ff8736bc54d1e5b2ad816022
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-10 00:25:11 -0500

    Control errors in read and write

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,30 +16,30 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97); }
 	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY | O_APPEND, 0664);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
 	count_rd = read(file_from, buf, 1024);
 	if (count_rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	error = close(file_from);
-	if (error == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100); }
-	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY | O_APPEND, 0664);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99); }
 	count_wt = write(file_to, buf, count_rd);
 	if (count_wt == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
+	error = close(file_from);
+	if (error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100); }
 	error = close(file_to);
 	if (error == -1)
 	{
