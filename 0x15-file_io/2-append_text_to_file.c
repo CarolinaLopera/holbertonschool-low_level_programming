fe2e7b748a7231af3843d667a4c80d46ddb4fc88
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-09 19:13:50 -0500

    Add lines at end the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_file - This function convert a binary to decimal.
+ * append_text_to_file - This function convert a binary to decimal.
  *
  * Return: Always int.
  * @filename: Is a variable char pointer.
@@ -8,15 +8,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int i;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
-    if (text_content == NULL)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
 		return (1);
 
-    for (i = 0; text_content[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 		;
-    write(*filename, text_content, i);
-    return (1);
+	write(*filename, text_content, i);
+	return (1);
 }
