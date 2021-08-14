d95fdaed420c31ae5788c0111abed3a938a8925a
CarolinaLopera <carolinalopera08@gmail.com>
2021-08-14 00:36:20 -0500

    Half fix int-min

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include "holberton.h"
+#include <limits.h>
 /**
 * print_number - This function print a number integer.
 *
@@ -7,14 +8,13 @@
 */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n < 0 && n != INT_MIN)
 	{
 		_putchar('-');
 		n = -n;
 	}
 	if (n / 10 != 0)
-	{
 		print_number(n / 10);
-	}
+
 	_putchar((n % 10) + '0');
 }
