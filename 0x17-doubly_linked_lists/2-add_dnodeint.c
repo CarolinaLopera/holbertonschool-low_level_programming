e2a64a1c758b0e67aabd7478065a14580975567d
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-17 14:42:25 -0500

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,12 +21,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->prev = NULL;
 
 	if (*head != NULL)
 		(*head)->prev = new_node;
-	else
-		new_node->prev = NULL;
+	(*head) = new_node;
 
 	return (new_node);
 }
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - Add a node at the end (tail) of a list.
+ *
+ * Return: Always a adress to new node.
+ * @head: is a pointer to the next node.
+ * @n: is a variable const int to add to the list.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{}
