484fae77250a5948d2631cdf196059bdb6c6edc5
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-17 14:24:32 -0500

    if head is null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,10 +18,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(new_node);
 		return (NULL);
 	}
+	if (head == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = (*head);
-
 	(*head) = new_node;
 	return (new_node);
 }
